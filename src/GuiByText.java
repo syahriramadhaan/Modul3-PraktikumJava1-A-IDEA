bb7ba162535ac5ba3b0a150300e6343d7036750f
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-13 11:59:24 +0800

    method menambahkanListener

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame {
 
     JLabel label1 = new JLabel("Coba Input 1");
-    JTextField textField =  new JTextField();
+    JTextField textField1 =  new JTextField();
     JButton button1 = new JButton("Greet");
 
     GuiByText(){
@@ -17,11 +19,11 @@ public class GuiByText extends JFrame {
         getContentPane().setLayout(null);
 
         getContentPane().add(label1);
-        getContentPane().add(textField);
+        getContentPane().add(textField1);
         getContentPane().add(button1);
 
         label1.setBounds(16,32,100,25);
-        textField.setBounds(124,32,100,25);
+        textField1.setBounds(124,32,100,25);
         button1.setBounds(232,32,80,25);
     }
 
@@ -29,5 +31,15 @@ public class GuiByText extends JFrame {
         GuiByText guiByText = new GuiByText();
         guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
+    }
+
+    void menambahkanListener(){
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String nama = textField1.getText();
+                JOptionPane.showMessageDialog(null,"Hai " + nama);
+            }
+        });
     }
 }
